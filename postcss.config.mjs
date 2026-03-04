f7a2d2ec0d23a9ec48228a1d0e89754f55a89c84
Ozod <zokirovozodxoja@gmail.com>
2026-03-04 22:24:30 +0500

    fix: postcss config for tailwind v3

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,3 @@
-const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
-};
-
-export default config;
+/** @type {import('postcss-load-config').Config} */ 
+const config = { plugins: { tailwindcss: {}, autoprefixer: {} } }; 
+export default config; 
